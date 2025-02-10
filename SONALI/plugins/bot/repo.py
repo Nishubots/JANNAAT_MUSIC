a438e8c4d8514b3dd3e7c08bd728ebfa10e937d8
DEVA <divyanshuchaudhary482@gmail.com>
2025-02-10 12:08:35 +0530

    Update repo.py

--- SONALI/plugins/bot/repo.py
+++ SONALI/plugins/bot/repo.py
@@ -11,9 +11,9 @@ start_txt = """
  
 ✰ 𝗥ᴇᴘᴏ ᴛᴏ 𝗡ʜɪ 𝗠ɪʟᴇɢᴀ 𝗬ʜᴀ
  
-✰ 𝗣ᴀʜʟᴇ 𝗣ᴀᴘᴀ 𝗕ᴏʟ 𝗥ᴇᴘᴏ 𝗢ᴡɴᴇʀ ᴋᴏ 
+✰ 𝐎𝐖𝐍𝐄𝐑 𝐒𝐄 𝐏𝐔𝐂𝐇𝐎 𝐑𝐄𝐏𝐎 𝐊𝐄 𝐋𝐈𝐘𝐄 
 
-✰ || @ll_RADHE7_ll ||
+✰ || @its_deva_heree  ||
  
 ✰ 𝗥ᴜɴ 24x7 𝗟ᴀɢ 𝗙ʀᴇᴇ 𝗪ɪᴛʜᴏᴜᴛ 𝗦ᴛᴏᴘ
  
@@ -29,14 +29,14 @@ async def start(_, msg):
           InlineKeyboardButton("𝗔ᴅᴅ 𝗠ᴇ 𝗕ᴀʙʏ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
         ],
         [
-          InlineKeyboardButton("𝗗𝗽_𝗰𝗼𝗹𝗹𝗲𝗰𝘁𝗶𝗼𝗻", url="https://t.me/DP_WORLD7"),
-          InlineKeyboardButton("𝗥𝗔𝗗𝗛𝗘", url="https://t.me/ll_RADHE7_ll"),
+          InlineKeyboardButton("˹𝚵𝗔𝗚𝗟𝗘˼ ˹𝗨𝗣𝗗𝗔𝗧𝚵𝗦˼", url="https://t.me/EAGLE_UPDTAES"),
+          InlineKeyboardButton("𝗥𝗔𝗗𝗛𝗘", url="https://t.me/its_deva_heree"),
           ],
                [
-                InlineKeyboardButton("𝗧ᴇᴀᴍ 𝗜ɴᴄʀɪᴄɪʙʟᴇ 𝗕ᴏᴛs", url=f"https://t.me/ll_BOTCHAMBER_ll"),
+                InlineKeyboardButton("𝐂𝐇𝐀𝐓 𝐆𝐑𝐎𝐔𝐏", url=f"https://t.me/UNDERGROUND_MAFIA_CLUB"),
 ],
 [
-InlineKeyboardButton("𝗠ᴀɪɴ 𝗕ᴏᴛ", url=f"https://t.me/RADHE_MUSIC_ROBOT"),
+InlineKeyboardButton("𝐌𝐔𝐒𝐈𝐂 𝐁𝐎𝐓", url=f"https://t.me/SUKOONN_MUSIC_BOT"),
 
         ]]
     
