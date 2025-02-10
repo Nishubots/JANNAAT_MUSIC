a167667e77f94e1075935de76804a9f07fdc10c5
DEVA <divyanshuchaudhary482@gmail.com>
2025-02-10 12:33:54 +0530

    Update play.py

--- SONALI/utils/inline/play.py
+++ SONALI/utils/inline/play.py
@@ -76,10 +76,10 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
       [
             InlineKeyboardButton(
-                text="• ᴜᴘᴅᴀᴛᴇ •", url ="https://t.me/ll_BOTCHAMBER_ll",
+                text="• ᴜᴘᴅᴀᴛᴇ •", url ="https://t.me/EAGLE_UPDTAES",
             ),
             InlineKeyboardButton(
-                text="• sᴜᴘᴘᴏʀᴛ •", url="https://t.me/FRIENDSHUBCHATZONE",
+                text="• sᴜᴘᴘᴏʀᴛ •", url="https://t.me/UNDERGROUND_MAFIA_CLUB",
             )
         ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -99,10 +99,10 @@ def stream_markup(_, videoid, chat_id):
          ],
         [
             InlineKeyboardButton(
-                text="• ᴜᴘᴅᴀᴛᴇ •", url="https://t.me/ll_BOTCHAMBER_ll",
+                text="• ᴜᴘᴅᴀᴛᴇ •", url="https://t.me/EAGLE_UPDTAES",
             ),
             InlineKeyboardButton(
-                text="• sᴜᴘᴘᴏʀᴛ •", url="https://t.me/FRIENDSHUBCHATZONE",
+                text="• sᴜᴘᴘᴏʀᴛ •", url="https://t.me/UNDERGROUND_MAFIA_CLUB",
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
